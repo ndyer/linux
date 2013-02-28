6094c0424708d0b8e3df1f461b6f0f896c972020
Nick Dyer <nick.dyer@itdev.co.uk>
2013-02-28 18:10:10 +0000

    Input: atmel_mxt_ts - Improve bootloader progress output
    By implementing a frame counter, print out fewer debug messages (the Atmel
    firmware may contain hundreds of frames).
    
    Signed-off-by: Nick Dyer <nick.dyer@itdev.co.uk>

--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -1404,6 +1404,7 @@ static int mxt_load_fw(struct device *dev, const char *fn)
 	unsigned int frame_size;
 	unsigned int pos = 0;
         unsigned int retry = 0;
+	unsigned int frame = 0;
 	int ret;
 
 	ret = request_firmware(&fw, fn, dev);
@@ -1469,12 +1470,16 @@ static int mxt_load_fw(struct device *dev, const char *fn)
                 } else {
                         retry = 0;
                         pos += frame_size;
+			frame++;
                 }
 
-		dev_dbg(dev, "Updated %d bytes / %zd bytes\n", pos, fw->size);
+		if (frame % 50 == 0)
+			dev_info(dev, "Sent %d frames, %d/%zd bytes\n",
+				 frame, pos, fw->size);
 	}
 
 	ret = mxt_wait_for_chg(data, MXT_FW_CHG_TIMEOUT);
+	dev_info(dev, "Sent %d frames, %zd bytes\n", frame, pos);
 
 out:
 	release_firmware(fw);
