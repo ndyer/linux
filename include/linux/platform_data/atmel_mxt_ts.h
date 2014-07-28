016614db139a77cf6dce65c4fc179067d8790fbe
Nick Dyer <nick.dyer@itdev.co.uk>
2014-07-28 10:14:34 -0700

    Input: atmel_mxt_ts - use deep sleep mode when stopped
    The hardcoded 0x83 CTRL setting overrides other settings in that byte,
    enabling extra reporting that may not be useful on a particular platform.
    
    Implement improved suspend mechanism via deep sleep. By writing zero to
    both the active and idle cycle times the maXTouch device can be put into a
    deep sleep mode, using minimal power. It is necessary to issue a calibrate
    command after the chip has spent any time in deep sleep, however a soft
    reset is unnecessary.
    
    Use the old method on Chromebook Pixel via platform data option.
    
    This patch also deals with the situation where the power configuration is
    zero on probe, which would mean that the device never wakes up to execute
    commands.
    
    After a config download, the T7 power configuration may have changed so it
    is necessary to re-read it.
    
    Signed-off-by: Nick Dyer <nick.dyer@itdev.co.uk>
    Acked-by: Benson Leung <bleung@chromium.org>
    Acked-by: Yufeng Shen <miletus@chromium.org>

--- include/linux/platform_data/atmel_mxt_ts.h
+++ include/linux/platform_data/atmel_mxt_ts.h
@@ -10,16 +10,18 @@
  * option) any later version.
  */
 
-#ifndef __LINUX_ATMEL_MXT_TS_H
-#define __LINUX_ATMEL_MXT_TS_H
+#ifndef __LINUX_PLATFORM_DATA_ATMEL_MXT_TS_H
+#define __LINUX_PLATFORM_DATA_ATMEL_MXT_TS_H
 
 #include <linux/types.h>
+#include <dt-bindings/input/atmel_mxt_ts.h>
 
 /* The platform data for the Atmel maXTouch touchscreen driver */
 struct mxt_platform_data {
 	unsigned long irqflags;
 	u8 t19_num_keys;
 	const unsigned int *t19_keymap;
+	enum mxt_suspend_mode suspend_mode;
 };
 
-#endif /* __LINUX_ATMEL_MXT_TS_H */
+#endif /* __LINUX_PLATFORM_DATA_ATMEL_MXT_TS_H */
